f12bd4ca8ac14bfe6e4195064dac3a4b234a4e44
Artem Gromov <artyomgromov@ya.ru>
2016-10-05 15:51:36 +0300

    Improve execution time using stream cursor

--- correcthorse.py
+++ correcthorse.py
@@ -1,18 +1,51 @@
 import random
+import sys
 
 filename = "20k.txt"
 
+
+def get_random_lines(file, num_of_lines):
+    last_char = file.seek(0, 2) - 1  # skipping EOF position with -1
+
+    lines = []
+    for _ in range(num_of_lines):
+        position = random.randint(0, last_char)
+        lines.append(get_line(file, position))
+
+    return lines
+
+
+def get_line(file, position):
+    start_position = position
+    while True:
+        file.seek(position)
+        symbol = file.read(1)
+        if symbol == '\n' and file.tell() != start_position + 1:  # get line when reaches \n, and when \n was not the first match
+            return file.readline().rstrip()
+
+        elif position == 0:                                       # get symbol + rest of the line when reaches start of file
+            return symbol + file.readline().rstrip()
+
+        else:
+            position -= 1
+
+
 def main(filename):
-	with open(filename) as words_file:
-		words = list(set(word.strip() for word in words_file))
-	while True:
-		length = raw_input("Words:")
-		try:
-			length = int(length)
-			print(" ".join(random.choice(words) for _ in range(length)))
-		except:
-			print("Please enter a valid number.")
+    with open(filename) as words_file:
+        while True:
+            try:
+                length = input('Enter number of words: ')
+                length = int(length)
+                print(' '.join(get_random_lines(words_file, length)))
+            except ValueError:
+                print('Please enter a valid integer number or use CTRL+c to exit.')
+            except KeyboardInterrupt:
+                print('\nHave a nice day!')
+                sys.exit(0)
+            except:
+                raise
+                sys.exit(1)
 
 
-if __name__ == "__main__":
-	main(filename)
+if __name__ == '__main__':
+    main(filename)
